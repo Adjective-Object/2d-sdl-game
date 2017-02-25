27170a92a5ab7500e963512f7d5176c31d3c2667
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2017-02-25 16:16:42 -0500

    linear sum approach to line-point intersection

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,8 @@
+#include <cmath>
 #include "engine/pair.hpp"
 #include "./util.hpp"
+
+#include <iostream>
 
 int checkLineIntersection(Pair const& p0,
                           Pair const& p1,
@@ -30,15 +33,100 @@ int checkLineIntersection(Pair const& p0,
     return 0;
 }
 
+
+
+// in this next function, we are considering a line that is sweeping,
+// from the initial line 'line1' passing through the two points p1 = (x1,y1), p2 = (x2,y2)
+// to the final line 'line2' passing through the two points p3 = (x3,y3) and p4 = (x4,y4)
+// there are two sweeping parameters: 
+//   't', which indicates how far along each line we are
+//   's', which indicates how far we are sweeping between line1 and line2 (the main sweeping parameter)
+// for instance:
+//  s=0 means we are on line1,
+//  s=1 means we are on line2,
+//  t=0 means we are on the line between p1 and p3,
+//  t=1 means we are on the line between p2 and p4
+// this function returns a specific value for each of t and s,
+// which correspond to when the swept line hits the origin O (at coordinates (0,0))
+// if either of the parameters is not between 0 and 1, this function instead returns null
+
+inline double _checkLineSweep(
+    double const& a1_x,
+    double const& a1_y,
+    double const& a2_x,
+    double const& a2_y,
+    double const& b1_x,
+    double const& b1_y,
+    double const& b2_x,
+    double const& b2_y
+    ) {
+
+    double rB = std::abs(b1_y) / (std::abs(b2_y) + std::abs(b1_y));
+    double rA = std::abs(a1_y) / (std::abs(a2_y) + std::abs(a1_y));
+
+    double B_x = rB * b1_x + (1 - rB) * b2_x;
+    double A_x = rA * a1_x + (1 - rA) * a2_x;
+
+    double rC = std::abs(A_x) / (std::abs(B_x) + std::abs(A_x));
+
+    return rC;
+}
+
 bool checkLineSweep(
-        Pair const& a1,
-        Pair const& a2,
-        Pair const& b1,
-        Pair const& b2,
+        Pair const& _a1,
+        Pair const& _a2,
+        Pair const& _b1,
+        Pair const& _b2,
         Pair const& c,
         Pair & out1,
         Pair & out2,
         double epsilon) {
-    return false;
-}
 
+    Pair a1, a2, b1, b2;
+
+    // exchange points if needed
+    double tmp;
+    if (_a1.y > _a2.y) {
+        a1 = _a2;
+        a2 = _a1;
+    } else {
+        a1 = _a1;
+        a2 = _a2;
+    }
+
+    // exchange points if needed
+    if (_b1.y > _b2.y) {
+        b1 = _b2;
+        b2 = _b1;
+    } else {
+        b1 = _b1;
+        b2 = _b2;
+    }
+
+    // set origin to c
+    double a1_x = a1.x - c.x;
+    double a1_y = a1.x - c.y;
+    double a2_x = a2.x - c.x;
+    double a2_y = a2.x - c.y;
+    double b1_x = b1.x - c.x;
+    double b1_y = b1.x - c.y;
+    double b2_x = b2.x - c.x;
+    double b2_y = b2.x - c.y;
+
+    double rC = _checkLineSweep(
+        a1_x,
+        a1_y,
+        a2_x,
+        a2_y,
+        b1_x,
+        b1_y,
+        b2_x,
+        b2_y
+    );
+
+    out1 = a1 * rC + b1 * (1 - rC);
+    out2 = a2 * rC + b2 * (1 - rC);
+
+
+    return true;
+};
--- tests/util.cpp
+++ tests/util.cpp
@@ -16,7 +16,7 @@ TEST(Util, checkLineIntersection_Basic) {
     EXPECT_EQ(Pair(1, 1), out);
 }
 
-TEST(Util, checkLineSweep_Basic) {
+TEST(Util, checkLineSweep_Horizontal) {
     //
     // a1      b1
     // |   .c  |
@@ -30,9 +30,137 @@ TEST(Util, checkLineSweep_Basic) {
 
     Pair out1, out2;
 
-    EXPECT_TRUE(checkLineSweep(a1, a2, b1, b2, c, out1, out2));
-    EXPECT_EQ(out1, Pair(1.5, 0));
-    EXPECT_EQ(out2, Pair(1.5, 1));
+    ASSERT_TRUE(checkLineSweep(a1, a2, b1, b2, c, out1, out2))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1) collides";
+    EXPECT_EQ(out1, Pair(1.5, 0))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+    EXPECT_EQ(out2, Pair(1.5, 1))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
 
 }
 
+TEST(Util, checkLineSweep_Horizontal_Miss) {
+    //
+    // a1      b1 
+    // |       |   .c
+    // a2      b2 
+    //
+
+    Pair a1 = Pair(1, 0), a2 = Pair(2, 0);
+    Pair b1 = Pair(1, 1), b2 = Pair(2, 1);
+
+    Pair c = Pair(3.5, 0.5);
+
+    Pair out1, out2;
+
+    EXPECT_FALSE(checkLineSweep(a1, a2, b1, b2, c, out1, out2));
+}
+
+
+TEST(Util, checkLineSweep_Horizontal_Miss_2) {
+    //
+    //      a1      b1
+    //  .c  |       |  
+    //      a2      b2
+    //
+
+    Pair a1 = Pair(2, 0), a2 = Pair(3, 0);
+    Pair b1 = Pair(2, 1), b2 = Pair(3, 1);
+
+    Pair c = Pair(0.5, 0.5);
+
+    Pair out1, out2;
+
+    EXPECT_FALSE(checkLineSweep(a1, a2, b1, b2, c, out1, out2));
+}
+
+
+TEST(Util, checkLineSweep_Horizontal_Reversed_Hit) {
+    //
+    //  a2      b1
+    //  |   .c  |
+    //  a1      b2
+    //
+
+    Pair a1 = Pair(1, 0), a2 = Pair(1, 1);
+    Pair b1 = Pair(2, 0), b2 = Pair(2, 1);
+
+    Pair c = Pair(1.5, 0.5);
+
+    Pair out1, out2;
+
+    ASSERT_TRUE(checkLineSweep(a2, a1, b1, b2, c, out1, out2))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1) collides";
+    EXPECT_EQ(out1, Pair(1.5, 0))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+    EXPECT_EQ(out2, Pair(1.5, 1))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+}
+
+TEST(Util, checkLineSweep_Horizontal_Reversed_Hit_2) {
+    //
+    //  a1      b2
+    //  |   .c  |
+    //  a2      b1
+    //
+
+    Pair a1 = Pair(1, 0), a2 = Pair(1, 1);
+    Pair b1 = Pair(2, 0), b2 = Pair(2, 1);
+
+    Pair c = Pair(1.5, 0.5);
+
+    Pair out1, out2;
+
+    ASSERT_TRUE(checkLineSweep(a1, a2, b2, b1, c, out1, out2))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1) collides";
+    EXPECT_EQ(out1, Pair(1.5, 0))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+    EXPECT_EQ(out2, Pair(1.5, 1))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+}
+
+TEST(Util, checkLineSweep_Horizontal_Reversed_Hit_3) {
+    //
+    //  a1      b2
+    //  |   .c  |
+    //  a2      b1
+    //
+
+    Pair a1 = Pair(1, 0), a2 = Pair(1, 1);
+    Pair b1 = Pair(2, 0), b2 = Pair(2, 1);
+
+    Pair c = Pair(1.5, 0.5);
+
+    Pair out1, out2;
+
+    ASSERT_TRUE(checkLineSweep(a2, a1, b2, b1, c, out1, out2))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1) collides";
+    EXPECT_EQ(out1, Pair(1.5, 0))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+    EXPECT_EQ(out2, Pair(1.5, 1))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+}
+
+
+TEST(Util, checkLineSweep_Vertical) {
+    // a1 ----- a2
+    //
+    //     .c
+    //
+    // a2 ----- b2
+
+    Pair a1 = Pair(1, 0), a2 = Pair(2, 0);
+    Pair b1 = Pair(1, 1), b2 = Pair(2, 1);
+
+    Pair c = Pair(1.5, 0.5);
+
+    Pair out1, out2;
+
+    ASSERT_TRUE(checkLineSweep(a1, a2, b1, b2, c, out1, out2))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1) collides";
+    EXPECT_EQ(out1, Pair(2, 0.5))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+    EXPECT_EQ(out2, Pair(1, 0.5))
+        << "line sweep (1,0)..(1,1) -> (2,0)..(2,1)";
+
+}
